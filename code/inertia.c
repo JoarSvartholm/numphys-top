5349ed57e350ed2d251b6a8aa6c3815bb17db47f
Joar <joar.svartholm@gmail.com>
2018-02-22 13:20:11 +0100

    test with top.o

--- code/inertia.c
+++ code/inertia.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include "integration.h"
 #include <math.h>
+#include "integration.h"
+#include "top.h"
 
 double rho_cyl(double x){
   return 1;
@@ -10,14 +11,22 @@ double y_cyl(double x){
   return 1;
 }
 
+double y_cone(double x){
+  return x;
+}
+
+double y_top1(double x){
+  return top_y(1,x);
+}
+
 int main(int argc, char const *argv[]) {
   double M,Ml;
 
-  M = integrate(1,rho_cyl,y_cyl,0,1);
-  Ml = integrate(2,rho_cyl,y_cyl,0,1);
+  M = integrate(1,rho_cyl,y_top1,0,1);
+  Ml = integrate(2,rho_cyl,y_cone,0,1);
 
   printf("Mass of unit cylinder is %f\n",M );
-  printf("analytical value: %f\n", M_PI );
+  printf("analytical value: %f\n", M_PI/3 );
 
   printf("COM of unit cylinder is %f\n",Ml );
   printf("analytical value: %f\n", .5);
---
+++ code/top.h
@@ -0,0 +1,19 @@
+/***
+    
+    Functions for the definition of a symmetric top 
+    
+    Given the distance z (in cm) to the tip of the top along the principal
+    axis of symmetry,
+    
+    top_y returns the distance to the principle axis of symmetry
+    
+    top_rho returns the mass density 
+    
+    for top number 1 to 4
+    
+***/
+
+double top_y (unsigned number, double z);
+
+double top_rho (unsigned number, double z);
+
